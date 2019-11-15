8cfefabbdf4d8786a52b094c126d1a7fe9c48f39
Apollo Tang <tang.apollo@gmail.com>
2019-11-15 01:38:36 -0500

    [redux explain] add another example for non premitive object

---
+++ setup-a-webpack/src/07-react-redux-explained/f--same-as-e-but-non-premitive-state/index.js
@@ -0,0 +1,148 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.createElement('div')
+container.id = 'app'
+document.body.appendChild(container)
+
+/* *********************************************************************************
+
+yarn dev src/07-counter-app-readering-redux-state/f--same-as-e-but-non-premitive-state/
+*********************************************************************************** */
+
+
+// App
+// ====
+const App = ({
+    dispatch_increase,
+    dispatch_decrease,
+    count
+}) => {
+  const handle_increase = () => {
+    dispatch_increase()
+  }
+
+  const handle_decrease = () => {
+    dispatch_decrease()
+  }
+  return (
+    <div>
+      <button onClick={handle_increase}>+</button>
+      <button onClick={handle_decrease}>-</button>
+      <div>{count}</div>
+    </div>
+  )
+}
+
+
+
+// Reducer
+// =======
+import {createStore} from 'redux'
+const initialState = {count:0}
+const reducer = (prevState=initialState, action) =>{
+  let nextState
+  switch (action.type) {
+    case 'inc':
+      nextState = {
+        ...prevState,
+        count: prevState.count + 1
+      }
+      break
+    case 'dec':
+      nextState = {
+        ...prevState,
+        count: prevState.count - 1
+      }
+      break
+    default:
+      nextState = prevState
+  }
+  return nextState
+}
+
+
+
+// Create store
+// ============
+const store = createStore(reducer)
+
+
+
+
+// Renderer
+// ========
+const render = (state) => {
+  // update store
+  const dispatch_increase = () => {
+    const action = {type:'inc'}
+    console.log(`action:    ${JSON.stringify(action)}`)
+    store.dispatch(action)
+  }
+
+  const dispatch_decrease = () => {
+    const action = {type:'dec'}
+    console.log(`action:    ${JSON.stringify(action)}`)
+    store.dispatch(action)
+  }
+
+
+  // select store
+  const count = state.count
+
+
+  // update virtual dom
+  ReactDOM.render(
+    <App
+      dispatch_increase={dispatch_increase}
+      dispatch_decrease={dispatch_decrease}
+      count={count}
+    />,
+    document.getElementById('app')
+  )
+}
+
+
+
+// Store subscription
+// ==================
+store.subscribe(function listener(){
+  const nextState = store.getState()
+  logger(initialState)(nextState)
+
+  render(nextState)  // <------ @@ push state into virtual dom @@
+})
+
+
+
+// Initial render, with initial state
+// ==================================
+render(store.getState())
+
+
+
+
+
+// Utility
+// ========
+function logger (initialState) {
+  return nextState => {
+    if (logger.prev) {
+      console.log(`prevState: ${JSON.stringify(logger.prev)}`)
+    } else {
+      console.log(`prevState: ${JSON.stringify(initialState)}`)
+    }
+    logger.prev = nextState
+    console.log(`nextState: ${JSON.stringify(nextState)}`)
+    console.log('=====================')
+  }
+}
+
+
+
+
+
+
+
+
+
--- setup-a-webpack/src/07-react-redux-explained/readme.md
+++ setup-a-webpack/src/07-react-redux-explained/readme.md
@@ -3,6 +3,7 @@
 
 To demo the steps:
 ```bash
+
 $ yarn dev src/07-counter-app-readering-redux-state/b--add-previous-counter-reduce-demo
 
 $ yarn dev src/07-counter-app-readering-redux-state/c--use-redux-library
@@ -10,6 +11,9 @@ $ yarn dev src/07-counter-app-readering-redux-state/c--use-redux-library
 $ yarn dev src/07-counter-app-readering-redux-state/d--rendering-state-w-react
 
 $ yarn dev src/07-counter-app-readering-redux-state/e--app-is-doing-the-dispatch
+
+$ yarn dev src/07-counter-app-readering-redux-state/f--same-as-e-but-non-premitive-state
+
 ```
 
 
